ef3c39ffbf7693e6a6d08a82e805c706e1d663bb
Scopped <coderdev23@gmail.com>
2025-03-27 15:05:55 +0100

    chore: Finished ZenKitPvPBootstrap

--- plugin/src/main/java/xyz/scopped/zenkitpvp/ZenKitPvPBootstrap.java
+++ plugin/src/main/java/xyz/scopped/zenkitpvp/ZenKitPvPBootstrap.java
@@ -2,15 +2,24 @@ package xyz.scopped.zenkitpvp;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public final class ZenKitPvPBootstrap extends JavaPlugin {
+
+    private ZenKitPvP plugin;
 
     @Override
     public void onEnable() {
-
+        try {
+            this.plugin = new ZenKitPvP(this);
+        } catch (Exception ex) {
+            getLogger().log(Level.SEVERE, "Failed to enable ZenKitPvP!", ex);
+            getServer().getPluginManager().disablePlugin(this);
+        }
     }
 
     @Override
     public void onDisable() {
-
+        if(this.plugin != null) this.plugin.disable();
     }
 }
